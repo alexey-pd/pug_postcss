13572254922eb4b74fe853dfa4371ad3891c0538
Alexey Pudnikov <alexey.pudnikov@yandex.ru>
2018-09-13 21:32:42 +0300

    latest updates and assets optimize

--- gulpfile.js
+++ gulpfile.js
@@ -1,112 +1,185 @@
 'use strict';
 
 const
-      gulp          = require('gulp'),
-      pjson         = require('./package.json'),
-      dirs          = pjson.config.directories,
-      ghPagesUrl    = pjson.config.ghPages,
-      postcss       = require('gulp-postcss'),
-      rename        = require('gulp-rename'),
-      del           = require('del'),
-      browserSync   = require('browser-sync').create(),
-      ghPages       = require('gulp-gh-pages'),
-      plumber       = require('gulp-plumber'),
-      pug           = require('gulp-pug'),
-      sourcemaps    = require('gulp-sourcemaps'),
-      cssnano       = require('cssnano'),
-      cssnext       = require('postcss-cssnext'),
-      mqpacker      = require('css-mqpacker'),
-      assets        = require('postcss-assets'),
-      flatten       = require('gulp-flatten'),
-      fs            = require('fs'),
-      atImport      = require('postcss-import'),
-      uglify        = require('gulp-uglify'),
-      concat        = require('gulp-concat'),
-      grid          = require('postcss-flexboxgrid'),
-      clearfix      = require('postcss-clearfix'),
-      data          = require('gulp-data');
+    gulp = require('gulp'),
+    pjson = require('./package.json'),
+    dirs = pjson.config.directories,
+    ghPagesUrl = pjson.config.ghPages,
+    postcss = require('gulp-postcss'),
+    rename = require('gulp-rename'),
+    del = require('del'),
+    browserSync = require('browser-sync').create(),
+    ghPages = require('gulp-gh-pages'),
+    plumber = require('gulp-plumber'),
+    pug = require('gulp-pug'),
+    sourcemaps = require('gulp-sourcemaps'),
+    cssnano = require('cssnano'),
+    cssnext = require('postcss-cssnext'),
+    mqpacker = require('css-mqpacker'),
+    assets = require('postcss-assets'),
+    flatten = require('gulp-flatten'),
+    fs = require('fs'),
+    atImport = require('postcss-import'),
+    concat = require('gulp-concat'),
+    uglify = require('gulp-uglify'),
+    svgSprite = require('gulp-svg-sprites'),
+    grid = require('postcss-flexboxgrid'),
+    clearfix = require('postcss-clearfix'),
+    data = require('gulp-data'),
+    range = require('postcss-input-range');
 
-      gulp.task('style', function(){
+gulp.task('scripts', function() {
+    gulp.src(dirs.src + dirs.scripts)
+        .pipe(plumber({
+            errorHandler: ''
+        }))
+        .pipe(concat(dirs.src + dirs.scripts))
+        .pipe(rename('app.min.js'))
+        .pipe(uglify())
+        .pipe(gulp.dest(dirs.dist + '/assets/scripts'))
+        .pipe(browserSync.stream());
+    gulp.src(dirs.src + dirs.scripts)
+        .pipe(concat(dirs.src + dirs.scripts))
+        .pipe(rename('app.js'))
+        .pipe(gulp.dest(dirs.dist + '/assets/scripts'))
+});
 
-         const
-          processors = [
+gulp.task('style', function() {
+
+    const
+        processors = [
             atImport(),
-            cssnext(),
-            grid({ columns: 12, gutter: 30 }),
-            clearfix(),
-            assets({
-              loadPaths: ['dist/assets/images/'],
-              relativeTo: 'dist/assets/styles/'
+            cssnext({
+                warnForDuplicates: false
             }),
-            mqpacker({ sort: true }),
-            cssnano()
-          ];
+            range(),
+            assets({
+                loadPaths: ['app/assets/images/'],
+                relativeTo: 'app/assets/styles/'
+            }),
+            mqpacker({
+                sort: true
+            }),
+            cssnano(),
+        ];
 
-          return gulp.src(dirs.src + dirs.styles)
-            .pipe(plumber({ errorHandler: '' }))
-            .pipe(concat('app.min.css'))
-            .pipe(sourcemaps.init())
-            .pipe(postcss(processors))
-            .pipe(sourcemaps.write('/'))
-            .pipe(gulp.dest(dirs.dist + '/assets/styles'))
-            .pipe(browserSync.stream());
-          });
+    return gulp.src(dirs.src + '/blocks/_blocks.css')
+        .pipe(plumber({
+            errorHandler: ''
+        }))
+        .pipe(sourcemaps.init())
+        .pipe(concat('app.min.css'))
+        .pipe(postcss(processors))
+        .pipe(sourcemaps.write('/'))
+        .pipe(gulp.dest(dirs.dist + '/assets/styles'))
+        .pipe(browserSync.stream());
+});
 
-      gulp.task('watch', function(){
-          gulp.watch( dirs.src + '/blocks/**/*.css', ['style'])
-          gulp.watch([dirs.src + '/blocks/**/*.pug', dirs.src + '/templates/**/*.pug'], ['pug'])
-      });
+gulp.task('watch', function() {
+    gulp.watch(dirs.src + '/blocks/**/*.pug', ['pug'])
+    gulp.watch(dirs.src + '/templates/**/*.pug', ['pug'])
+    gulp.watch(dirs.src + '/blocks/**/*.css', ['style'])
+    gulp.watch(dirs.src + '/blocks/**/*.js', ['scripts'])
+    gulp.watch(dirs.src + '/blocks/**/*.svg', ['svg-sprites'])
 
-      gulp.task('default', ['pug', 'style', 'copyPics', 'watch', 'browser-sync']);
+});
 
-      gulp.task('del', function () {
-        console.log('app clean');
-        return del([
-          dirs.dist + '/**/*'
-        ]);
-      });
+gulp.task('default', ['pug', 'style', 'scripts', 'copyAssets', 'svg-sprites', 'watch', 'browser-sync']);
+gulp.task('build', ['pug', 'style', 'scripts', 'copyAssets', 'svg-sprites']);
 
-      gulp.task('delBlocks', function () {
-        console.log('blocks clean');
-        return del([
-          dirs.blocks + '/**/*'
-        ]);
-      });
+gulp.task('del', function() {
+    console.log('app clean');
+    return del([
+        dirs.dist + '/**/*'
+    ]);
+});
 
-      gulp.task('browser-sync', function(){
+function delBlocksImports() {
+    return new Promise((resolve, reject) => {
+        fs.writeFile(`src/templates/_layout/_includes/blocks.pug`, ``, err => {
+            if (err) {
+                reject(`ERR>>> Failed to rewrite blocks.pug`);
+            } else {
+                resolve();
+            }
+        });
+    });
+}
 
-          browserSync.init({
-            server: {
-              baseDir: "./app/"
-              },
-              port: 3000,
-              startPath: '/index.html',
-              // proxy: "gulp.dev"
-            });
-      });
+function createStyleCore(){
+  return new Promise((resolve, reject) => {
+      fs.writeFile(`src/blocks/_blocks.css`, ``, err => {
+        if (err) {
+            reject(`ERR>>> Failed to rewrite blocks.css`);
+        } else {
+            resolve();
+        }
+    });
+  });
+}
 
-      gulp.task('reload', function(){
-          browserSync.reload();
-      });
+gulp.task('delBlocks', function() {
+    console.log('blocks clean');
+    delBlocksImports();
+    del([
+        dirs.blocks + '/**/*'
+    ])
+    return createStyleCore();
+});
 
-      gulp.task('pug', function() {
+gulp.task('browser-sync', function() {
 
-        return gulp.src(dirs.src + '/templates/*.pug')
-          .pipe(plumber({ errorHandler: '' }))
-          .pipe(data(file => require(dirs.src + '/data.json')))
-          .pipe(pug())
-          .pipe(gulp.dest(dirs.dist));
-          browserSync.stream();
-          browserSync.reload();
-      });
+    browserSync.init({
+        server: {
+            baseDir: "./app/"
+        },
+        port: 3000,
+        startPath: '/index.html',
+    });
+});
 
-      gulp.task('copyPics', function() {
-        gulp.src(dirs.src + '/blocks/**/*.{png,jpg,jpeg}')
-          .pipe(flatten({ includeParents: 0 }))
-          .pipe(gulp.dest(dirs.dist + '/assets/images'));
-      });
+gulp.task('reload', function() {
+    browserSync.reload();
+});
 
-      gulp.task('pages', function() {
-        return gulp.src('./app/**/*')
-          .pipe(ghPages());
-      });
+gulp.task('pug', function() {
+
+    return gulp.src(dirs.src + '/templates/*.pug')
+        .pipe(plumber({
+            errorHandler: ''
+        }))
+        .pipe(data(file => require(dirs.src + '/data.json')))
+        .pipe(pug({
+            pretty: true
+        }))
+        .pipe(gulp.dest(dirs.dist));
+    browserSync.stream();
+    browserSync.reload();
+});
+
+gulp.task('copyAssets', function() {
+    gulp.src(dirs.src + '/blocks/**/*.{png,jpg,jpeg}')
+        .pipe(flatten({
+            includeParents: 0
+        }))
+        .pipe(gulp.dest(dirs.dist + '/assets/images'));
+    gulp.src(dirs.src + '/fonts/**/*.{eot,woff,ttf,woff2}')
+        .pipe(flatten({
+            includeParents: 1
+        }))
+        .pipe(gulp.dest(dirs.dist + '/assets/fonts'));
+});
+
+gulp.task('pages', function() {
+    console.log(ghPagesUrl);
+    return gulp.src('./app/**/*')
+        .pipe(ghPages());
+});
+
+
+gulp.task('svg-sprites', function() {
+    console.log(dirs.dist + '/assets');
+    return gulp.src(dirs.src + dirs.svgContent)
+        .pipe(svgSprite({ mode: "symbols" }))
+        .pipe(gulp.dest(dirs.src + '/templates/_layout/_includes/svg-sprite'))
+});
--- newBlock.js
+++ newBlock.js
@@ -1,10 +1,10 @@
 'use strict';
 
 const
-  fs   = require('fs'),
-  path = require('path'),
-  readline = require('readline'),
-  createInterface = readline.createInterface;
+    fs = require('fs'),
+    path = require('path'),
+    readline = require('readline'),
+    createInterface = readline.createInterface;
 
 const rl = createInterface(process.stdin, process.stdout);
 
@@ -16,126 +16,153 @@ const fileSources = {
 };
 
 function validateBlockName(blockName) {
-	return new Promise((resolve, reject) => {
-		const isValid = /^(\d|\w|-)+$/.test(blockName);
+    return new Promise((resolve, reject) => {
+        const isValid = /^(\d|\w|-)+$/.test(blockName);
 
-		if (isValid) {
-			resolve(isValid);
-		} else {
-			const errMsg = (
-				`ERR>>> An incorrect block name '${blockName}'\n` +
-				`ERR>>> A block name must include letters, numbers & the minus symbol.`
-			);
-			reject(errMsg);
-		}
-	});
+        if (isValid) {
+            resolve(isValid);
+        } else {
+            const errMsg = (
+                `ERR>>> An incorrect block name '${blockName}'\n` +
+                `ERR>>> A block name must include letters, numbers & the minus symbol.`
+            );
+            reject(errMsg);
+        }
+    });
 }
 
 function directoryExist(blockPath, blockName) {
-	return new Promise((resolve, reject) => {
-		fs.stat(blockPath, notExist => {
-			if (notExist) {
-				resolve();
-			} else {
-				reject(`ERR>>> The block '${blockName}' already exists.`);
-			}
-		});
-	});
+    return new Promise((resolve, reject) => {
+        fs.stat(blockPath, notExist => {
+            if (notExist) {
+                resolve();
+            } else {
+                reject(`ERR>>> The block '${blockName}' already exists.`);
+            }
+        });
+    });
 }
 
 function createDir(dirPath) {
-	return new Promise((resolve, reject) => {
-		fs.mkdir(dirPath, err => {
-			if (err) {
-				reject(`ERR>>> Failed to create a folder '${dirPath}'`);
-			} else {
-				resolve();
-			}
-		});
-	});
+    return new Promise((resolve, reject) => {
+        fs.mkdir(dirPath, err => {
+            if (err) {
+                reject(`ERR>>> Failed to create a folder '${dirPath}'`);
+            } else {
+                resolve();
+            }
+        });
+    });
 }
 
 function createFiles(blocksPath, blockName) {
-	const promises = [];
-	Object.keys(fileSources).forEach(ext => {
-		const fileSource = fileSources[ext].replace(/\{blockName}/g, blockName);
-		const filename = `${blockName}.${ext}`;
-		const filePath = path.join(blocksPath, filename);
+    const promises = [];
+    Object.keys(fileSources).forEach(ext => {
+        const fileSource = fileSources[ext].replace(/\{blockName}/g, blockName);
+        const filename = `${blockName}.${ext}`;
+        const filePath = path.join(blocksPath, filename);
 
-		promises.push(
-				new Promise((resolve, reject) => {
-					fs.writeFile(filePath, fileSource, 'utf8', err => {
-						if (err) {
-							reject(`ERR>>> Failed to create a file '${filePath}'`);
-						} else {
-							resolve();
-						}
-					});
-				})
-		);
-	});
+        promises.push(
+            new Promise((resolve, reject) => {
+                fs.writeFile(filePath, fileSource, 'utf8', err => {
+                    if (err) {
+                        reject(`ERR>>> Failed to create a file '${filePath}'`);
+                    } else {
+                        resolve();
+                    }
+                });
+            })
+        );
+    });
 
-	return Promise.all(promises);
+    return Promise.all(promises);
 }
 
 function getFiles(blockPath) {
-	return new Promise((resolve, reject) => {
-		fs.readdir(blockPath, (err, files) => {
-			if (err) {
-				reject(`ERR>>> Failed to get a file list from a folder '${blockPath}'`);
-			} else {
-				resolve(files);
-			}
-		});
-	});
+    return new Promise((resolve, reject) => {
+        fs.readdir(blockPath, (err, files) => {
+            if (err) {
+                reject(`ERR>>> Failed to get a file list from a folder '${blockPath}'`);
+            } else {
+                resolve(files);
+            }
+        });
+    });
+}
+
+function pugConnect(blockName) {
+    return new Promise((resolve, reject) => {
+        fs.appendFile(`src/templates/_layout/_includes/blocks.pug`, `//- block ${blockName} \ninclude ../../../blocks/${blockName}/${blockName}.pug \n\n`, err => {
+            if (err) {
+                reject(`ERR>>> Failed to rewrite blocks.pug`);
+            } else {
+                resolve();
+            }
+        });
+    });
+}
+
+function styleConnect(blockName) {
+    return new Promise((resolve, reject) => {
+        fs.appendFile(`src/blocks/_blocks.css`, `/* block ${blockName} */ \n@import "${blockName}/${blockName}.css";\n\n`, err => {
+            if (err) {
+                reject(`ERR>>> Failed to rewrite blocks.pug`);
+            } else {
+                resolve();
+            }
+        });
+    });
 }
 
 function printErrorMessage(errText) {
-	console.log(errText);
-	rl.close();
+    console.log(errText);
+    rl.close();
 }
 
 function initMakeBlock(candidateBlockName) {
-	const blockNames = candidateBlockName.trim().split(/\s+/);
+    const blockNames = candidateBlockName.trim().split(/\s+/);
 
-	const makeBlock = blockName => {
-		const blockPath = path.join(BLOCKS_DIR, blockName);
+    const makeBlock = blockName => {
+        const blockPath = path.join(BLOCKS_DIR, blockName);
 
-		return validateBlockName(blockName)
-			.then(() => directoryExist(blockPath, blockName))
-			.then(() => createDir(blockPath))
-			.then(() => createFiles(blockPath, blockName))
-			.then(() => getFiles(blockPath))
-			.then(files => {
-				const line = '-'.repeat(48 + blockName.length);
-				console.log(line);
-				console.log(`The block has just been created in 'src/blocks/${blockName}'`);
-				console.log(line);
+        return validateBlockName(blockName)
+            .then(() => directoryExist(blockPath, blockName))
+            .then(() => createDir(blockPath))
+            .then(() => createFiles(blockPath, blockName))
+            .then(() => getFiles(blockPath))
+            .then(files => {
+                const line = '-'.repeat(48 + blockName.length);
+                console.log(line);
+                console.log('');
+                console.log(`Компонент ${blockName} создан`);
+                console.log(line);
 
-				files.forEach(file => console.log(file));
+                files.forEach(file => console.log(file));
 
-				rl.close();
-			});
-	};
+                rl.close();
+            })
+            .then(() => pugConnect(blockName))
+            .then(() => styleConnect(blockName))
+    };
 
-	if (blockNames.length === 1) {
-		return makeBlock(blockNames[0]);
-	}
+    if (blockNames.length === 1) {
+        return makeBlock(blockNames[0]);
+    }
 
-	const promises = blockNames.map(name => makeBlock(name));
-	return Promise.all(promises);
+    const promises = blockNames.map(name => makeBlock(name));
+    return Promise.all(promises);
 }
 
 const blockNameFromCli = process.argv
-		.slice(2)
-		.join(' ');
+    .slice(2)
+    .join(' ');
 
 if (blockNameFromCli !== '') {
-	initMakeBlock(blockNameFromCli).catch(printErrorMessage);
+    initMakeBlock(blockNameFromCli).catch(printErrorMessage);
 } else {
-	rl.setPrompt('Block(s) name: ');
-	rl.prompt();
-	rl.on('line', (line) => {
-		initMakeBlock(line).catch(printErrorMessage);
-	});
+    rl.setPrompt('Block(s) name: ');
+    rl.prompt();
+    rl.on('line', (line) => {
+        initMakeBlock(line).catch(printErrorMessage);
+    });
 }
